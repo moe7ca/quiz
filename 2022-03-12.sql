b603929bdb10b1befc9a81e87a68b090af6ae94d
Ahmed Noori <moe7ca@gmail.com>
2022-03-12 06:12:41 -0500

    quiz

---
+++ 2022-03-12.sql
@@ -0,0 +1,5 @@
+public.objectives (id, question, answer) FROM stdin;
+1 What is a WHERE clause?	A query can filter or restrict the information that results via a WHERE clause.
+2 What is a query?	A query is a task executed against a database, table or combination of tables. e.g. SELECT, INSERT, etc.
+3 What is a table?	A table is a set of data collected as columns and rows.	1
+4 How do you set a maximum width on HTML elements?	.these_elements { max-width: 135px; }
